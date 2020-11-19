5e4ccf35f60cb41e33eb8b2c85b3b705b5143d22
APTEM <semen.tombov@yandex.ru>
2020-11-19 13:09:40 +0300

    Git и желтые окружности

--- first.py
+++ first.py
@@ -1,1 +1,37 @@
-print('My first git-repo')
+import sys
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Рисование')
+        uic.loadUi('krygi.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_figure(qp)
+        qp.end()
+
+    def draw_figure(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        w = randint(0, 200)
+        qp.drawEllipse(randint(100, 700) - w // 2, randint(100, 350) - w // 2, w, w)
+
+    def paint(self):
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
