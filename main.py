21cce7dea4032954dd06f5fc31d4e234c87db8d3
ApTMaLt <74650232+ApTMaLt@users.noreply.github.com>
2020-11-19 21:10:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,16 @@
 import sys
+from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from random import randint
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(280, 470, 181, 71))
-        self.pushButton.setObjectName("pushButton")
-        self.line = QtWidgets.QFrame(self.centralwidget)
-        self.line.setGeometry(QtCore.QRect(0, 450, 801, 16))
-        self.line.setFrameShadow(QtWidgets.QFrame.Plain)
-        self.line.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line.setObjectName("line")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Рисовать"))
-
-
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Рисование')
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -52,16 +18,12 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         qp.begin(self)
         self.draw_figure(qp)
         qp.end()
-
     def draw_figure(self, qp):
-        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-        w = randint(0, 225)
+        qp.setBrush(QColor(255, 255, 0))
+        w = randint(0, 200)
         qp.drawEllipse(randint(100, 700) - w // 2, randint(100, 350) - w // 2, w, w)
-
     def paint(self):
         self.repaint()
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
